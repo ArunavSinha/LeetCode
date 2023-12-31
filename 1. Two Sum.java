b404a464eac8b6d4763a7c95bbc15f5c33615958
Arunav Sinha <asinh060@uottawa.ca>
2023-12-31 13:09:15 -0500

    Added optimal solution (hashmap) to problem 1

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -9,4 +9,21 @@ class Solution {
         }
         return new int[]{};
     }
+
+    public int[] TwoSum(int[] nums, int target) { // my Optimized solution (HashMap)
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++){
+            map.put(nums[i], i);
+        }
+
+        for (int j = 0; j < n; j++){
+            int complement = target - nums[j];
+            if (map.containsKey(complement) && map.get(complement) != j){
+                return new int[]{j, map.get(complement)};
+            }
+        }
+        return new int[]{};
+    }
 }
