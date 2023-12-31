98fc8b026ad38ca97633155fd95a1ce9ed03dd4e
Arunav Sinha <asinh060@uottawa.ca>
2023-12-31 01:52:39 -0500

    Solved problem 1 - Two Sum (brute force)

---
+++ 1. Two Sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) { // my brute force solution
+        for (int i = 0; i <= nums.length-1; i++){
+            for (int j = nums.length-1; j >= 0; j--){
+                if (((nums[i] + nums[j]) == target) && (i != j)){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
